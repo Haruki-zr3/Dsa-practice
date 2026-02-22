55cd3af58d88c5fa33de856a6344494e9a4526f6
Harsh Kr. Singh <mslko9859@gmail.com>
2026-02-22 19:21:48 +0530

    Add sortColors function to sort red, white, and blue
    Implement Dutch National Flag algorithm to sort colors.

---
+++ sort_colors.cpp
@@ -0,0 +1,23 @@
+//I used Dutch National Flag Algo , why colors
+//we need to sort red,white and blue just like The Flag, so we numbered them as 0,1 and 2
+class Solution {
+public:
+    void sortColors(vector<int>& arr) {
+        int n=arr.size();
+        int low=0,mid=0,high=n-1;
+        while(mid<=high){
+            if(arr[mid]==0){
+                swap(arr[low],arr[mid]);
+                low++;
+                mid++;
+            }
+            else if(arr[mid]==1){
+                mid++;
+            }
+            else if(arr[mid]==2){
+                swap(arr[mid],arr[high]);
+                high--;
+            }
+        }
+    }
+};
