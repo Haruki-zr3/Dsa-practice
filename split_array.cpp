d3d3ff21f9cc39c6a8308c275b42109c5c3b9ec4
Harsh Kr. Singh <mslko9859@gmail.com>
2026-04-03 19:04:50 +0530

    Implement solution for splitting array with constraints

---
+++ split_array.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool canSplit(vector<int>& nums, int k, long long maxSum) {
+        int count = 1;
+        long long currentSum = 0;
+        for (int num : nums) {
+            if (currentSum + num > maxSum) {
+                count++;
+                currentSum = num;
+                if (count > k) return false;
+            } else {
+                currentSum += num;
+            }
+        }
+        return true;
+    }
+    int splitArray(vector<int>& nums, int k) {
+        long long left = *max_element(nums.begin(), nums.end());
+        long long right = accumulate(nums.begin(), nums.end(), 0LL);
+        long long ans = right;
+        while (left <= right) {
+            long long mid = left + (right - left) / 2;
+
+            if (canSplit(nums, k, mid)) {
+                ans = mid;
+                right = mid - 1; 
+            } else {
+                left = mid + 1; 
+            }
+        }
+        return ans;
+    }
+};
